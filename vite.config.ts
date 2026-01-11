a0436774ec94e7f3f7decf693ecd288b42e3a15e
irsalshydiq <144502106+Irs622@users.noreply.github.com>
2026-01-12 01:01:26 +0700

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   build: {
-    outDir: "dist"
-  }
-})
+    target: "esnext",
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
